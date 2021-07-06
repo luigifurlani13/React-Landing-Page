b92edd3fe7e2676bdbe8309b91300fcded42525f
luigifurlani13 <luis.furlan@icloud.com>
2021-07-06 05:18:30 +0000

    resetted the home.js and index.js files

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Card } from "./component/card.js";
+import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Card />, document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#app"));
